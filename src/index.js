5abe307fb2aa3378933466cf52ae619df515ff2b
Ahmad Smith <void@ahmads-air.mynetworksettings.com>
2024-11-17 22:04:41 -0500

    Script receiving valid response from api

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+export default async function lambdaSearch(sku, context = () => {}) {
+    const url = "https://2fwotdvm2o-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia%20for%20JavaScript%20(4.14.3)%3B%20Browser%20(lite)%3B%20JS%20Helper%20(3.11.1)%3B%20react%20(18.2.0)%3B%20react-instantsearch%20(6.38.1)&x-algolia-api-key=10cf0ae11ca33a8c7c80a8c6bd926bdf&x-algolia-application-id=2FWOTDVM2O";
+
+    var options = {
+        url: "https://2fwotdvm2o-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia%20for%20JavaScript%20(4.14.3)%3B%20Browser%20(lite)%3B%20JS%20Helper%20(3.11.1)%3B%20react%20(18.2.0)%3B%20react-instantsearch%20(6.38.1)&x-algolia-api-key=10cf0ae11ca33a8c7c80a8c6bd926bdf&x-algolia-application-id=2FWOTDVM2O",
+        data: `{
+            "requests": [{
+                "indexName": "product_variants_v2_flight_club",
+                "params": "distinct=true&facets=%5B%5D&highlightPostTag=%3C%2Fais-highlight-0000000000%3E&highlightPreTag=%3Cais-highlight-0000000000%3E&hitsPerPage=10&query=${sku}&tagFilters="
+            }]
+        }`,
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; rv:20.0) Gecko/20121202 Firefox/20.0'
+        }
+    };
+    try {
+        const response = await axios.post(url, options.data, {headers: options.headers });
+        if (response.data && response.data.results && response.data.results[0]) {
+            const hits = response.data.results[0].hits;
+            console.log("Search Results Hits:", hits);
+            context(null, hits);
+        }
+    } catch (error) {
+            if (!error.response) {
+                let err = new Error(`Could not connect to Flight Club while searching for ${sku}.` + "Error:", error);
+                console.error(err);
+                context(err);
+            } else {
+                let statusCode = error.response.status;
+                let err = new Error(`Could not connect to Flight Club while searching for ${sku} - Status Code: ${statusCode}`);
+                console.error(err);
+                context(err)
+            }
+        }  
+}
+
+
+lambdaSearch("hq4540");
